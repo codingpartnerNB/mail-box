c0b7529fd70b13445e08775f3d1c8d00ba623660
codingpartnerNB <nehab5314@gmail.com>
2024-01-25 00:38:52 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import RootLayout from './components/Layout/RootLayout';
 import Signup from './components/Signup/Signup';
+import Home from './components/Layout/Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<RootLayout />}>
           <Route index element={<Signup />} />
+          <Route path="home" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/Layout/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = ()=>{
+    return(
+        <React.Fragment>
+            <p className="text-center text-purple-700">Welcome to your Mail Box</p>
+        </React.Fragment>
+    );
+}
+
+export default Home;
